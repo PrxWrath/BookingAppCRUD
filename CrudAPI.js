17355dc666200835b98f203aa2c5d33f2825dfcd
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2022-11-07 11:26:22 +0530

    Added features to post and get users from crudcrud

--- CrudAPI.js
+++ CrudAPI.js
@@ -1,13 +1,22 @@
 const form = document.querySelector('.myForm');
 const msg = document.querySelector('.msg');
 const btn = document.querySelector('#submit-btn');
-
 const container = document.querySelector(".container");
 
 form?.addEventListener('submit', onSubmit);
 
-//display users in local storage when document is loaded
-//document.addEventListener('DOMContentLoaded', displayUsers(0));
+document.addEventListener('DOMContentLoaded', ()=>{
+    
+    //GET users from crudcrud and populate the users display
+    axios.get('https://crudcrud.com/api/f16610c0a5934895b1933c7b7f6bb388/userData')
+        .then(res => {
+            for (let user of res.data) {
+                displayUsers(user);
+            }
+        })
+        .catch(err => { console.error(err) });
+});
+
 
 //add new users
 function onSubmit(e) {
@@ -29,13 +38,14 @@ function onSubmit(e) {
             phone: ph.value
         }
 
-        axios.post('https://crudcrud.com/api/650b16b3c5bf41c591b834bf5296be0d/userData', user)
-            .then(res=>{console.log(res)})
-            .catch(err=>{console.error(err)});
+        //POST users to crudcrud
+        axios.post('https://crudcrud.com/api/f16610c0a5934895b1933c7b7f6bb388/userData', user)
+            .then((res) => { displayUsers(res.data) })
+            .catch(err => { console.error(err) });
 
         uname.value = '';
         email.value = '';
-        ph.value = '' ;
+        ph.value = '';
 
     }
 
@@ -45,41 +55,38 @@ function onSubmit(e) {
 //display existing users   
 function displayUsers(user) {
 
+    
     let uname = document.querySelector('#name');
     let email = document.querySelector('#email');
     let ph = document.querySelector('#phone')
 
+    let newDiv = document.createElement('div');
+    newDiv.className = 'list-group-item m-auto bg-dark text-light w-75';
+    newDiv.setAttribute('id', user.email);
 
-        let newDiv = document.createElement('div');
-        newDiv.className = 'list-group-item m-auto bg-dark text-light w-75';
-        newDiv.setAttribute('id', user.email);
+    let editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-sm btn-primary mr-2 float-right';
+    editBtn.textContent = 'Edit'
+    editBtn.addEventListener('click', (e) => {
+        uname.value = user.name;
+        email.value = user.email;
+        ph.value = user.phone;
+        //features to be added in next task
+    })
 
-        let editBtn = document.createElement('button');
-        editBtn.className = 'btn btn-sm btn-primary mr-2 float-right';
-        editBtn.textContent = 'Edit'
-        editBtn.addEventListener('click', (e) => {
-            uname.value = user.name;
-            email.value = user.email;
-            ph.value = user.phone;
-            newDiv.remove();
-            localStorage.removeItem(`User${user.email}`)
-        })
+    let delBtn = document.createElement('button');
+    delBtn.textContent = 'X'
+    delBtn.className = 'btn btn-sm btn-danger float-right';
+    delBtn.addEventListener('click', (e) => {
+        //features to be added in next task
+    })
 
-        let delBtn = document.createElement('button');
-        delBtn.textContent = 'X'
-        delBtn.className = 'btn btn-sm btn-danger float-right';
-        delBtn.addEventListener('click', (e) => {
-            newDiv.remove();
-            localStorage.removeItem(`User${user.email}`)
-        })
+    let data = document.createTextNode(user.name + ": " + user.email);
+    newDiv.appendChild(data);
+    newDiv.appendChild(delBtn);
+    newDiv.appendChild(editBtn);
 
-        let data = document.createTextNode(user.name + ": " + user.email);
-        newDiv.appendChild(data);
-        newDiv.appendChild(delBtn);
-        newDiv.appendChild(editBtn);
-
-        container.appendChild(newDiv);
-    
+    container.appendChild(newDiv);
 }
 
 
